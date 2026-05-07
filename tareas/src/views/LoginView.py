4e8661628cc407d96e08a16c3ae94520dcc81587
LuisAvila28 <23308060610140@cetis61.edu.mx>
2026-05-06 19:51:36 -0600

    Refactor LoginView for improved UI and functionality
    Updated the login view to enhance UI elements and improve user experience.

--- tareas/src/views/LoginView.py
+++ tareas/src/views/LoginView.py
@@ -1,67 +1,120 @@
 import flet as ft
-from views.RegistroView import RegistroView
-from datetime import datetime
-
 
 def LoginView(page: ft.Page, auth_controller):
     
-    def ver_contra():
-        contra.password = not contra.password
-        contra.update()
-        
-    correo=(ft.TextField(label="Correo",autofocus=True, icon=ft.Icons.PERSON ))
-    contra=(ft.TextField(label="Contraseña",suffix=ft.IconButton(icon=ft.Icons.VISIBILITY, on_click=ver_contra) ,password=True, autofocus=True, icon=ft.Icons.PASSWORD))
-    
-    
-    
+    correo = ft.TextField(
+        label="Correo electrónico",
+        prefix_icon=ft.Icons.PERSON,
+        width=400,
+        border_radius=10,
+        keyboard_type=ft.KeyboardType.EMAIL,
+        border_color=ft.Colors.GREEN_300
+    )
+
+    contraseña = ft.TextField(
+        label="Contraseña",
+        prefix_icon=ft.Icons.KEY,
+        password=True,
+        can_reveal_password=True,
+        width=400,
+        border_radius=10,
+        border_color=ft.Colors.GREEN_300
+    )
+
+    mensaje = ft.Text("", color=ft.Colors.GREEN_300)
+
+    def mostrar_snackbar(mensaje_texto, color=ft.Colors.GREEN_400):
+        page.snack_bar = ft.SnackBar(
+            content=ft.Text(mensaje_texto),
+            bgcolor=color,
+            duration=2000,
+        )
+        page.snack_bar.open = True
+        page.update()
+
     def login_click(e):
-        if not correo.value or not contra.value:
-            page.show_dialog(ft.SnackBar(ft.Text("Por favor, complete todos los campos")))
+        if not correo.value or not contraseña.value:
+            mensaje.value = "Por favor, llene todos los campos"
+            mensaje.color = ft.Colors.GREEN_300
+            page.update()
             return
         
-    
-        user, msg = auth_controller.login(correo.value, contra.value)
-    
+        user, msg = auth_controller.login(correo.value, contraseña.value)
         if user:
             page.user_data = user
+            mostrar_snackbar("¡Sesión iniciada correctamente!", ft.Colors.GREEN_400)
             page.go("/dashboard")
         else:
-            page.show_dialog(ft.SnackBar(ft.Text(msg)))
-            
-    def olvidado():
-        page.show_dialog(ft.SnackBar(ft.Text("Se a enviado su contraseña al correo")))
-    
-    def registro():
-        page.go("/registro")
-    
-    iniciar=( ft.Button("Iniciar sesion",color=ft.Colors.WHITE ,bgcolor=ft.Colors.BLUE,on_click=login_click))
-    registrarse =( ft.TextButton("¿Quieres registrarte?", on_click=registro))
-    olvidada =( ft.TextButton("¿Olvidaste la contraseña?", on_click=olvidado))
-    
+            mensaje.value = msg
+            mensaje.color = ft.Colors.GREEN_300
+            page.update()
+
+    iniciar_sesion = ft.ElevatedButton(
+        "Iniciar sesión",
+        width=250,
+        on_click=login_click,
+        style=ft.ButtonStyle(
+            bgcolor=ft.Colors.GREEN_400,
+            color=ft.Colors.WHITE,
+            padding=20,
+            shape=ft.RoundedRectangleBorder(radius=12),
+        ),
+    )
+
+    btn_registro = ft.TextButton(
+        "¿No tienes cuenta? Regístrate",
+        on_click=lambda _: page.go("/register"),
+        style=ft.ButtonStyle(
+            color=ft.Colors.GREEN_400
+        )
+    )
+
+    contraseña.on_submit = login_click
+
     return ft.View(
         route="/",
-        vertical_alignment=ft.MainAxisAlignment.CENTER, 
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         appbar=ft.AppBar(
-            title=ft.Text("Login"),
-            bgcolor=ft.Colors.BLUE_GREY_900,
-            color="white"
+            title=ft.Text("SIGE - Inicio Sesion"),
+            bgcolor=ft.Colors.GREEN_400,
+            color=ft.Colors.WHITE
         ),
         controls=[
             ft.Column(
                 [
-                    ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.Colors.BLUE),
-                    ft.Text("Acceso al sistema", size=24, weight="bold"),
+                    ft.Icon(
+                        icon=ft.Icons.AUTO_AWESOME,
+                        size=60,
+                        color=ft.Colors.GREEN_400
+                    ),
+
+                    ft.Container(height=10),
+
+                    ft.Text("Inicia Sesion", size=24, weight="bold", color=ft.Colors.GREEN_400),
+                    ft.Container(height=10),
+
                     correo,
-                    contra,
-                    iniciar,
-                    registrarse,
-                    olvidada
+                    ft.Container(height=10),
+
+                    contraseña,
+                    ft.Container(height=10),
+
+                    mensaje,
+                    ft.Container(height=10),
+
+                    ft.Row(
+                        [iniciar_sesion],
+                        alignment=ft.MainAxisAlignment.CENTER
+                    ),
+
+                    ft.Container(height=10),
+
+                    btn_registro
                 ],
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=20,
-                tight=True 
+                tight=True,
+                spacing=10
             )
         ]
     )
-    
